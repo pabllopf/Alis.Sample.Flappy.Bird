5e37e43f69a2c507e090ca22fafc83356c14156e
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2026-03-08 11:33:52 +0000

    fix: new autogenerate files to desktop and web runtimes

--- alis.sample.flappy.bird/Game.cs
+++ alis.sample.flappy.bird/Game.cs
@@ -1,33 +1,3 @@
-// --------------------------------------------------------------------------
-// 
-//                               █▀▀█ ░█─── ▀█▀ ░█▀▀▀█
-//                              ░█▄▄█ ░█─── ░█─ ─▀▀▀▄▄
-//                              ░█─░█ ░█▄▄█ ▄█▄ ░█▄▄▄█
-// 
-//  --------------------------------------------------------------------------
-//  File:Program.cs
-// 
-//  Author:Pablo Perdomo Falcón
-//  Web:https://www.pabllopf.dev/
-// 
-//  Copyright (c) 2021 GNU General Public License v3.0
-// 
-//  This program is free software:you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-// 
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
-//  GNU General Public License for more details.
-// 
-//  You should have received a copy of the GNU General Public License
-//  along with this program.If not, see <http://www.gnu.org/licenses/>.
-// 
-//  --------------------------------------------------------------------------
-
-using System.Reflection;
 using Alis.Core.Aspect.Math.Definition;
 using Alis.Core.Ecs;
 using Alis.Core.Ecs.Components.Audio;
@@ -38,18 +8,11 @@ using Alis.Core.Physic.Dynamics;
 
 namespace Alis.Sample.Flappy.Bird
 {
-    /// <summary>
-    ///     The program class
-    /// </summary>
-    public static class Program
+    public static class Game
     {
-        /// <summary>
-        ///     Main the args
-        /// </summary>
-        /// <param name="args">The args</param>
-        public static void Main(string[] args)
+        public static VideoGame Create()
         {
-            VideoGame
+             return    VideoGame
                 .Create()
                 .Settings(setting => setting
                     .General(general => general
@@ -485,7 +448,7 @@ namespace Alis.Sample.Flappy.Bird
                             ) // end bird 
                         ) // end scene manager
                     ) // end video game
-                .Run();
+                .Build();
         }
     }
 }
